bdf8d075fde01151c528ead6ec4d67f4655e3daf
darkwizard242 <am900820@gmail.com>
2020-11-29 15:35:56 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,51 +6,95 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE = 'nodejs'
+PACKAGE_BINARY = '/usr/bin/node'
+NPM_BINARY = '/usr/bin/npm'
+NPX_BINARY = '/usr/bin/npx'
+DEBIAN_REPO_FILE = '/etc/apt/sources.list.d/nodejs.list'
+EL_REPO_FILE = '/etc/yum.repos.d/nodejs.repo'
+
+
 def test_nodejs_package_installed(host):
+    """
+    Tests if nodejs is installed.
+    """
     assert host.package("nodejs").is_installed
 
 
 def test_nodejs_binary_exists(host):
-    assert host.file('/usr/bin/node').exists
+    """
+    Tests if nodejs binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_nodejs_binary_file(host):
-    assert host.file('/usr/bin/node').is_file
-
-
-def test_npm_binary_exists(host):
-    assert host.file('/usr/bin/npm').exists
-
-
-def test_npm_binary_file(host):
-    assert host.file('/usr/bin/npm').is_file
-
-
-def test_npx_binary_exists(host):
-    assert host.file('/usr/bin/npx').exists
-
-
-def test_npx_binary_file(host):
-    assert host.file('/usr/bin/npx').is_file
-
-
-def test_nodejs_repo_exists(host):
-    assert host.file('/etc/apt/sources.list.d/nodejs.list').exists or \
-      host.file('/etc/yum.repos.d/nodejs.repo').exists
-
-
-def test_nodejs_repo_file(host):
-    assert host.file('/etc/apt/sources.list.d/nodejs.list').is_file or \
-      host.file('/etc/yum.repos.d/nodejs.repo').is_file
+    """
+    Tests if nodejs binary is a file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_nodejs_binary_which(host):
-    assert host.check_output('which node') == '/usr/bin/node'
+    """
+    Tests the output to confirm nodejs's binary location.
+    """
+    assert host.check_output('which node') == PACKAGE_BINARY
+
+
+def test_npm_binary_exists(host):
+    """
+    Tests if npm binary exists.
+    """
+    assert host.file(NPM_BINARY).exists
+
+
+def test_npm_binary_file(host):
+    """
+    Tests if npm binary is a file type.
+    """
+    assert host.file(NPM_BINARY).is_file
 
 
 def test_npm_binary_which(host):
+    """
+    Tests the output to confirm npm's binary location.
+    """
     assert host.check_output('which npm') == '/usr/bin/npm'
 
 
+def test_npx_binary_exists(host):
+    """
+    Tests if npx binary exists.
+    """
+    assert host.file(NPX_BINARY).exists
+
+
+def test_npx_binary_file(host):
+    """
+    Tests if npx binary is a file type.
+    """
+    assert host.file(NPX_BINARY).is_file
+
+
 def test_npx_binary_which(host):
-    assert host.check_output('which npx') == '/usr/bin/npx'
+    """
+    Tests the output to confirm npx's binary location.
+    """
+    assert host.check_output('which npx') == NPX_BINARY
+
+
+def test_nodejs_repo_exists(host):
+    """
+    Tests if nodejs repo exists on DEBIAN/EL Systems.
+    """
+    assert host.file(DEBIAN_REPO_FILE).exists or \
+        host.file(EL_REPO_FILE).exists
+
+
+def test_nodejs_repo_file(host):
+    """
+    Tests if nodejs repo is a file type on DEBIAN/EL Systems.
+    """
+    assert host.file(DEBIAN_REPO_FILE).is_file or \
+        host.file(EL_REPO_FILE).is_file
